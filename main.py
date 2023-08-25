586093f95ffe7709dc6713124889c3b05d4af439
Danya Dyakonov <danila-dyakonov@mail.ru>
2023-08-25 23:06:10 +0300

    Edit function display_entries and edit_entry

--- main.py
+++ main.py
@@ -16,30 +16,31 @@ def create_entry():
         file.writelines(new_entry + '\n')
 
 
-def display_entry(entries:list):
+def display_entries(entries:list):
     entry_on_page = 2
     for count in range(0, len(entries),entry_on_page):
         page = entries[count:count + entry_on_page]
         for entry in page:
             print(entry)
         if len(entries) > entry_on_page:
-            print('Продолжить')
+            print('________________________________________')
 
 
 def edit_entry(line_in_handbook:int):
-    if line_in_handbook > len(entries):
+    try:
+        edit_line = entries[line_in_handbook].split()
+        edit_element = input('Выберите что бы Вы хотели изменить: ')
+        new_element = input('ВВедите новую запись: ')
+        index = edit_line.index(edit_element)
+        edit_line[index] = new_element
+        string = ' '.join(edit_line)
+        entries[line_in_handbook] = string
+        print(entries)
+        with open(handbook_information,'w') as file:
+            for entry in entries:
+                file.write(entry + '\n')
+    except IndexError:
         print('Такой записи не существует')
-    edit_line = entries[line_in_handbook].split()
-    edit_element = input('Выберите что бы Вы хотели изменить: ')
-    new_element = input('ВВедите новую запись: ')
-    index = edit_line.index(edit_element)
-    edit_line[index] = new_element
-    string = ' '.join(edit_line)
-    entries[line_in_handbook] = string
-    print(entries)
-    with open(handbook_information,'w') as file:
-        for entry in entries:
-            file.write(entry + '\n')
 
 
 def search_entries():
@@ -51,31 +52,35 @@ def search_entries():
                 if entry not in result:
                     result.append(entry)
 
-    display_entry(result) if result else print('Таких записей нет(((')
+    display_entries(result) if result else print('Таких записей нет(((')
 
 
+#Загрузка существующих записией из файла
 if os.path.exists(handbook_information):
     with open(handbook_information, 'r') as file:
         entries = [line.strip() for line in file.readlines()]
 else:
     entries = []
 
-print('1 - Добавить запись')
-print('2 - Редактировать запись')
-print('3 - Вывести записи постранично')
-print('4 - Поиск записей по параметрам')
 
-choice = int(input('Ввыберите действие'))
+if __name__ == "__main__":
+    print('1 - Добавить запись')
+    print('2 - Редактировать запись')
+    print('3 - Вывести записи постранично')
+    print('4 - Поиск записей по параметрам')
 
-match choice:
-    case 1:
-        create_entry() # Функция на создание записи
-    case 2:
-        print('Количество записей')
-        edit_entry(2) # Для редактирования записи нужно передать номер записи (Обратите внимание на количество записей)
-    case 3:
-        display_entry(entries)  # Функция на отображение постранично
-    case 4:
-        search_entries() # В функии на поиск записей нужно будет указать по каким характеристик\ам Вы собираетесь искать
+    choice = int(input('Выберите действие: '))
+
+    match choice:
+        case 1:
+            create_entry() # Функция на создание записи
+        case 2:
+            print('Количество записей',len(entries))
+            index = int(input('Введите запись, которую хотите отредактировать: '))
+            edit_entry(index) # Для редактирования записи нужно передать номер записи (Обратите внимание на количество записей)
+        case 3:
+            display_entries(entries)  # Функция на отображение постранично
+        case 4:
+            search_entries() # В функии на поиск записей нужно будет указать по каким характеристик\ам Вы собираетесь искать
 
 
